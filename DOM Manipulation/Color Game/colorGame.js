8787f37bf888d5b360f0f169dd7d429a7f773a0c
Pratik Kshirsagar <pratikkshirsagar@Pratiks-MBP.hsd1.il.comcast.net>
2018-06-26 17:05:39 -0500

    Refactored Color Game JavaScript code.

--- DOM Manipulation/Color Game/colorGame.js
+++ DOM Manipulation/Color Game/colorGame.js
@@ -1,67 +1,23 @@
-// 
-
-var colors = generateRandomColors(6);
-var gameMode = "hard";
-var pickedColor = colorPicker(6);
+var colors = [];
+var gameMode = "";
+var pickedColor = "";
 var message = document.body.querySelector("#message");
 var h1 = document.querySelector("h1");
 var squares = document.querySelectorAll(".square");
 var resetButton = document.querySelector("#reset");
-// var easyBtn = document.querySelector("#EasyBtn");
-// var hardBtn = document.querySelector("#HardBtn");
 var mode = document.querySelectorAll(".mode");
+var colorDisplay = document.querySelector("#pickedColor");
 
-function colorPicker(num){
-	var randomColor = Math.floor(Math.random() * num);
-	return colors[randomColor];
+init();
+
+function init(){
+	gameMode = "hard";
+	resetGame(6);
 }
 
-// for(i=0; i < colors.length; i++) {
-// 	//add initiall colors
-// 	squares[i].style.backgroundColor = colors[i];
-// 	//add click listeners.
-// 	squares[i].addEventListener("click", function(){
-// 		if (this.style.backgroundColor === pickedColor) {
-// 			changeColor(pickedColor);
-// 			message.textContent = "Correct!";
-// 			h1.style.backgroundColor = pickedColor;
-// 		}
-// 		else{
-// 			this.style.backgroundColor = "#232323";
-// 			message.textContent = "Try Again!";
-// 		}
-// 	})
-// }
-
-resetColors();
-
-function changeColor(color){
-	for(i=0; i < squares.length; i++){
-		squares[i].style.backgroundColor = color;
-	}
-}
-
-document.querySelector("#pickedColor").textContent = pickedColor;
-
-function generateRandomColors(num) {
-	var arr = [];
-	for(i = 0; i < num; i++) {
-		arr.push(randomColorGen());
-	}
-	return arr;
-}
-
-function randomColorGen() {
-	// "rgb(255, 0, 0)"
-	var r = Math.floor(Math.random() * 256);
-	var g = Math.floor(Math.random() * 256);
-	var b = Math.floor(Math.random() * 256);
-	var colorString = "rgb(" + String(r) + ", " + String(g) + ", " + String(b) + ")";
-	return colorString;  
-}
 
 resetButton.addEventListener("click", function(){
-	if (gameMode === "easy") ? resetGame(3) : resetGame(60);
+	gameMode === "easy" ? resetGame(3) : resetGame(6);
 });
 
 for(i = 0; i < mode.length; i++ ){	
@@ -87,26 +43,33 @@ for(i = 0; i < mode.length; i++ ){
 	});
 }
 
+function colorPicker(num){
+	var randomColor = Math.floor(Math.random() * num);
+	return colors[randomColor];
+}
 
-// easyBtn.addEventListener("click", function(){
-// 	gameMode = "easy";
-// 	easyBtn.classList.add("selected");
-// 	hardBtn.classList.remove("selected");
-// 	resetGame(3);
-// 	for(i = 3; i < 6; i++){
-// 		squares[i].style.display = "none";
-// 	}
-// });
+function changeColor(color){
+	for(i=0; i < squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+}
 
-// hardBtn.addEventListener("click", function(){
-// 	gameMode = "hard";
-// 	easyBtn.classList.remove("selected");
-// 	hardBtn.classList.add("selected");
-// 	resetGame(6);
-// 	for(i = 3; i < 6; i++){
-// 		squares[i].style.display = "block";
-// 	}
-// });
+function generateRandomColors(num) {
+	var arr = [];
+	for(i = 0; i < num; i++) {
+		arr.push(randomColorGen());
+	}
+	return arr;
+}
+
+function randomColorGen() {
+	// "rgb(255, 0, 0)"
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	var colorString = "rgb(" + String(r) + ", " + String(g) + ", " + String(b) + ")";
+	return colorString;  
+}
 
 function resetColors(){
 	for(i=0; i < colors.length; i++) {
@@ -136,5 +99,5 @@ function resetGame(num){
 	resetColors();
 	h1.style.backgroundColor = "steelblue";
 	resetButton.textContent = "New Colors";
-	document.querySelector("#pickedColor").textContent = pickedColor;
+	colorDisplay.textContent = pickedColor;
 }
