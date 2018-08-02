09db6509ce24a8f0e559260a68510e865e419873
Pratik Kshirsagar <pratikkshirsagar@Pratiks-MBP.hsd1.il.comcast.net>
2018-08-02 15:24:09 -0500

    YelpCamp v9

---
+++ YelpCamp/v9/routes/campgrounds.js
@@ -0,0 +1,68 @@
+var express = require("express"),
+    Campground = require("../models/campground"),
+    router  = express.Router();
+
+// INDEX ROUTE - show all campgrounds
+router.get("/", function(req, res) {
+    //get all camgrounds from db
+    Campground.find({}, function(err, campgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds/index", {campgrounds: campgrounds});
+        }
+    });
+    
+});
+
+
+//CREATE ROUTE - add a new campground from form
+    router.post("/", isLoggedIn, function(req, res){
+    //create new campground from obtained data.
+    var name = req.body.name;
+    var image = req.body.image;
+    var description = req.body.description;
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    };
+    var newCampground = {name: name, image: image, description: description, author: author};
+    Campground.create(newCampground, function(err, newCampground){
+         if(err){
+             console.log(err);
+         }  else{
+             res.redirect("/");
+         }
+    });
+});
+
+//NEW ROUTE - form to add a new campground
+router.get("/new", isLoggedIn, function(re, res){
+    //render form page.
+    res.render("campgrounds/new");
+});
+
+//SHOW ROUTE - show a particular campground in detail
+router.get("/:id", function(req, res) {
+//   res.send("This will be the show page one day!"); 
+//find the campground with provided id
+//render show template of that campground
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        }   else{
+            res.render("campgrounds/show", {campground: foundCampground});
+        }
+    });
+    
+});
+
+//middleware
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+module.exports = router;
